26b1717996732d7bd6fb30fb9382e95ea6b2e997
mcancetin <cmuhammed5300@gmail.com>
2024-02-04 22:59:36 +0300

    Update imports in searchbar.jsx

--- src/components/common/searchbar/searchbar.jsx
+++ src/components/common/searchbar/searchbar.jsx
@@ -9,9 +9,9 @@ import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
-import DatePicker from "@mui/x-date-pickers/DatePicker";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import LocalizationProvider from "@mui/x-date-pickers/LocalizationProvider";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 import useFetch from "../../../hooks/useFetch";
 
